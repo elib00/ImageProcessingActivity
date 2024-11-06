8fc5ccfc4b9c74af46d58f70b3fdcb01eb9fd47c
elib00 <joshuanapinas19@gmail.com>
2024-11-06 14:10:42 +0800

    [Joshua] Updated

--- ImageProcessingActivity/Form1.cs
+++ ImageProcessingActivity/Form1.cs
@@ -12,9 +12,103 @@ namespace ImageProcessingActivity
 {
     public partial class Form1 : Form
     {
+        Bitmap loaded, processed;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            loaded = new Bitmap(openFileDialog1.FileName);
+            pictureBox1.Image = loaded;
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            processed.Save(saveFileDialog1.FileName);
+        }
+
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pixelCopyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            processed = new Bitmap(loaded.Width, loaded.Height);
+            Color pixel;
+            for (int y = 0; y < loaded.Height; y++)
+            {
+                for (int x = 0; x < loaded.Width; x++)
+                {
+                    pixel = loaded.GetPixel(x, y);
+                    processed.SetPixel(x, y, pixel);
+                }
+            }
+
+            pictureBox2.Image = processed;
+        }
+
+        private void inversionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            processed = new Bitmap(loaded.Width, loaded.Height);
+            Color pixel;
+            for (int y = 0; y < loaded.Height; y++)
+            {
+                for (int x = 0; x < loaded.Width; x++)
+                {
+                    pixel = loaded.GetPixel(x, y);
+                    Color gray = Color.FromArgb(255 - pixel.R, pixel.G, pixel.B);
+                    processed.SetPixel(x, y, gray);
+                }
+            }
+
+            pictureBox2.Image = processed;
+        }
+
+        private void grayscalingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            processed = new Bitmap(loaded.Width, loaded.Height);
+            Color pixel;
+            int average;
+            for (int y = 0; y < loaded.Height; y++)
+            {
+                for (int x = 0; x < loaded.Width; x++)
+                {
+                    pixel = loaded.GetPixel(x, y);
+                    average = (int) (pixel.R + pixel.G + pixel.B) / 3;
+                    Color gray = Color.FromArgb(average, average, average);
+                    processed.SetPixel(x, y, gray);
+                }
+            }
+
+            pictureBox2.Image = processed;
         }
     }
 }
